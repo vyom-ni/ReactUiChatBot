b53255776025189dd9be89ad0e40a793379def4c
archana nayak <shannu.parkala@gmail.com>
2025-07-02 01:35:29 +0530

    FINAL

--- backend3/app.py
+++ backend3/app.py
@@ -19,9 +19,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["*"], 
     allow_headers=["*"],
 )
 
